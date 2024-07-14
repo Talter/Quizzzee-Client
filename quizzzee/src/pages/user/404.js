d4c99a90a9b177d69ab88539d964bee10c0235e4
Talter <ToryNothing@gmail.com>
2024-07-15 06:21:00 +0700

    Sawconed - Final Commit

--- quizzzee/src/pages/user/404.js
+++ quizzzee/src/pages/user/404.js
@@ -3,13 +3,13 @@ import notfound from "../../images/system/NotFound.png";
 
 function NotFound() {
   return (
-    <div className="relative">
+    <div className="min-h-screen py-0  text-white flex justify-center pt-24">
       <img
         src={notfound}
         alt="404 not found"
-        className="absolute top-0 scale-50"
+        className="w-1/2 h-1/2"
       />
-    </div>
+      </div>
   );
 }
 
